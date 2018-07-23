a1b85ebfe0cde960fd189b28ee6881892a003915
shinyoung-jung <shinyoung-jung@172.30.1.50>
2018-07-23 11:29:40 +0900

    d

---
+++ 2018.07_LuceneTest/src/main/java/tom/test/lucenetest/NewBranchFile.java
@@ -0,0 +1,12 @@
+package tom.test.lucenetest;
+
+public class NewBranchFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//NewB
+	}
+
+}
---
+++ 2018.07_LuceneTest/src/main/java/tom/test/lucenetest/OldBranchFile.java
@@ -0,0 +1,12 @@
+package tom.test.lucenetest;
+
+public class OldBranchFile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		//MASTER Version
+	}
+
+}
